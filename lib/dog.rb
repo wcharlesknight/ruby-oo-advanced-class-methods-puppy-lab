bf922a14b399a4e379b8d0f5fc068bd88b375c32
Charlie Knight <williamchknight@gmail.com>
2020-11-19 11:54:59 -0800

    reworked this whole code. final

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    
+    attr_accessor :name, :clear
+    attr_reader :save
+
+    @@all = []
+    def initialize(name)
+        @name = name 
+        @save = save 
+        #binding.pry    
+    end
+
+    def save
+        @@all << self  
+    end 
+    
+    def self.all
+        @@all
+        #binding.pry
+    end 
+
+    def self.print_all
+        @@all.each { |d| puts d.name }
+    end
+    
+    def self.clear_all
+        @@all.clear
+     end 
+
+end
